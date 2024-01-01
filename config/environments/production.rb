a288aed4f3cda1d95753048b93607474ef92976a
sediqwe <sediqwe@gmail.com>
2024-01-01 21:41:13 +0100

    To Server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
     'x-frame-options' => 'sameorigin'
   }
   config.hosts << "sediqwe.eu"
-  GA.tracker = "G-5Y9BZL1H1F"
+ 
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
