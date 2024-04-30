606f4b0771ca0aa9e7407c57fe426225ccc75b99
Sediqwe <sediqwe@gmail.com>
2024-04-30 19:09:49 +0200

    Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'login', to: 'devise/sessions#create'
+    get 'logout', to: 'devise/sessions#destroy'
+    get 'useredit', to: 'devise/registrations#edit'
+  end
+  devise_for :user, controllers: {
+        registrations: 'users/registrations'
+      }
+
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
