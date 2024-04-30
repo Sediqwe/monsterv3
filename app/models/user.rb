efb43bbc6e434f63e628d0dc30af52e58826ea45
Sediqwe <sediqwe@gmail.com>
2024-04-30 21:23:40 +0200

    Kisbetüs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ApplicationRecord
+  before_validation :normalize_username
+  before_validation :normalize_email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  private
+
+  def normalize_username
+    self.username = username.strip.downcase unless username.nil?
+  end
+  def normalize_email
+    self.email = email.strip.downcase unless email.nil?
+  end
          
 end
