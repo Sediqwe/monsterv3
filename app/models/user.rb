063233aca5b978df35f846061cb5757f27267e22
Sediqwe <sediqwe@gmail.com>
2024-05-01 09:11:26 +0200

    rememberme

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   before_validation :normalize_username
   before_validation :normalize_email
-  before_save :create_remember_token
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
